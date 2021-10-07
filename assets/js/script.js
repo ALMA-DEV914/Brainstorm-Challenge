3a1afae1051641e1742cdbc88ed32af36ba40724
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-07 16:42:06 -0700

    Add more link text and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,63 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('timer');
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
 
-var number =
-  '30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0';
-var timer = number.split(" ");
-
+var answers = ['3', '1', '2', '1', '3'];
+var questions = "";
+for (var i = 0; i < questions.length; i++) {
+    answers += questions[i];
+var answers = function() {
+    for (var i = 0; i < questions.length; i++) {
+        answers += question1[i];
+        if (answers === ckecked){
+            alert("Congrats! you got it right!");
+        } else if (question1 != checked){
+            alert("Your answer is wrong, 5 seconds is deducted from your time left.");
+            timeLeft = timeLeft - 5;
+        } else{
+            localStorage.setItem("checked", JSON.stringify("checked"));
+        }
+        localStorage.getItem("answers", JSON.parse("answers"));
+    };
+  }
+}
 function countdown() {
-  var timeLeft = 5;
+  var timeLeft = 30;
 
   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
-    timerEl.textContent = "";
+    timerEl.textContent = timeLeft;
     timeLeft--;
     if(timeLeft < 0){
-     clearInterval(timeInterval)
-    }
-     displayTimer();
-
-  }, 2000);
+        clearInterval(timeInterval);
+     } 
+    
+     
+}, 2000);
   
 }
+countdown();
 
-mainEl.setAttribute("style", "color: #8a0b1c; font-size: 25px;");
-
-// Displays the number one word at a time
-function displayTimer() {
-    var numberCount = 0;
+timerEl.setAttribute("style", "color: #8a0b1c; font-size: 25px;");
+mainEl.setAttribute("style", "color: blue;");
   
-    // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var queInterval = setInterval(function() {
-      
-      if (timer[numberCount] === undefined) {
-        // Use `clearInterval()` to stop the timer
-        clearInterval(queInterval);
-      } else {
-        mainEl.textContent = timer[numberCount];
-        numberCount++;
-      }
-    }, 2000);
-  }
-  
-  countdown();
 
   function myFunction(){
     onclick = document.getElementById("que").style.display = "none";
     onclick = document.getElementById("que1").style.display = "block";
-      
- }
+    onclick = document.getElementById("question2").style.display = "none";
+  }
+  function myFunction2(){
+      onclick = document.getElementById("question1").style.display = "none";
+     onclick = document.getElementById("que1").style.display = "block";
+    onclick = document.getElementById("question2").style.display = "block";
+  }
+function myFunction3(){
+    onclick = document.getElementById("que").style.display = "none";
+    onclick = document.getElementById("question2").style.display = "none";
+    onclick = document.getElementById("que1").style.display = "block";
+    onclick = document.getElementById("question3").style.display = "block";
+  }
+  
+ 
