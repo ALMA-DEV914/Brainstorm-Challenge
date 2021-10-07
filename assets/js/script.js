345fe3ad77117caf72922e815481bb4e625a248f
ALMA_DEV914 <aungonalna58@gamil.com>
2021-10-07 02:07:49 -0700

    Add section for the 1st question and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,34 @@ var mainEl = document.getElementById('timer');
 
 var number =
   '30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0';
-var timer = number.split(' ');
+var timer = number.split(" ");
 
 function countdown() {
-  var timeLeft = "";
+  var timeLeft = 5;
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
-    timerEl.textContent = timeLeft;
+    timerEl.textContent = "";
     timeLeft--;
-    if(timeLeft < 1){
+    if(timeLeft < 0){
      clearInterval(timeInterval)
     }
      displayTimer();
-  }, 1000);
 
+  }, 1000);
+  
 }
 
 mainEl.setAttribute("style", "color: #8a0b1c; font-size: 25px;");
-// Displays the message one word at a time
+
+// Displays the number one word at a time
 function displayTimer() {
     var numberCount = 0;
   
     // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var queInterval = setInterval(function() {
       
-      if (timer[numberCount] === 0) {
+      if (timer[numberCount] === undefined) {
         // Use `clearInterval()` to stop the timer
         clearInterval(queInterval);
       } else {
@@ -39,3 +41,9 @@ function displayTimer() {
   }
   
   countdown();
+
+  function myFunction(){
+    onclick = document.getElementById("que").style.display = "none";
+    onclick = document.getElementById("que1").style.display = "block";
+      
+ }
